156aacecc0550c10f9e03da5ee9be159bfdab0eb
Callum Ingham <csf23inc>
2024-09-24 15:23:45 +0100

    Implemented code

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1;
+int num2;
+int result;
+char type;
+bool invalidType = false;
+
+Console.WriteLine("Enter your first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Would you like to add (+), subtract (-), multiply (*), or divide (/) these two numbers?");
+type = Convert.ToChar(Console.ReadLine());
+
+switch (type)
+{
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        result = num1 / num2;
+        break;
+    default:
+        invalidType = true;
+        result = 0;
+        break;
+}
+
+if (invalidType)
+{
+    Console.WriteLine("Invalid math type " + type + ".");
+}
+else
+{
+    Console.WriteLine("Your calculation is:\n" + num1 + " " + type + " " + num2 + " = " + result + ".");
+}
